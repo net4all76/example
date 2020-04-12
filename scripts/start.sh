64baa10952530d1a87204419b36ad4787cff68a5
net4all76 <learningmind@gmail.com>
2020-04-13 00:34:32 +0900

    non-stop deploy

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
 ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname  $ABSPATH)
+ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=example
 
 echo "> Copy build file"
-
+echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> New application deploy"
-
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo  "> JAR NAME : $JAR_NAME "
@@ -27,6 +26,6 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+        -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
         -Dspring.profiles.active=$IDLE_PROFILE \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
