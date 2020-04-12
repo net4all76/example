4839677d409bddb4aeb2f9e2043617f37446af47
net4all76 <learningmind@gmail.com>
2020-04-12 11:03:37 +0900

    switch jar

---
+++ scripts/profile.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+#find idle profile: if real1 is running, real2 is idle.  On the other side, real1 is idle.
+
+function find_idle_profile() {
+
+  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+
+  if [ ${RESPONSE_CODE} -ge 400 ]
+  then
+    CURRENT_PROFILE=real2
+  else
+    CURRENT_PROFILE=$(curl -s http://localhost/profile)
+  fi
+
+  if [ ${CURRENT_PROFILE} == real1 ]
+  then
+    IDLE_PROFILE=real2
+    ]
+  else
+    IDLE_PROFILE=real
+  fi
+
+  echo "${IDLE_PROFILE}"
+}
+
+# find idle profile's port
+function find_idle_port(){
+  IDLE_PROFILE=$(find_idle_profile)
+
+  if [ ${IDLE_PROFILE} == real1 ]
+  then
+    echo "8081"
+  else
+    echo "8082"
+  fi
+
+}
---
+++ scripts/stop.sh
@@ -0,0 +1,16 @@
+ABSDIR=$(dirname  $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+IDLE_PORT=$(find_idle_port)
+
+echo "> check running application pid on $IDLE_PORT "
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
+
+if [ -z ${IDLE_PID}]
+then
+    echo "> Current application is not running. It can't terminate."
+else
+    echo "> kill -15 $IDLE_PID"
+    kill -15 ${IDLE_PID}
+    sleep 5
+fi
