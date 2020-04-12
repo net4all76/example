840ea8ae79658ed3a7eb0383f4d6c7681a7690e2
net4all76 <learningmind@gmail.com>
2020-04-13 02:01:40 +0900

    test profile

---
+++ src/test/java/com/jojoldu/book/springboot/web/ProfileControllerTest.java
@@ -0,0 +1,29 @@
+package com.jojoldu.book.springboot.web;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerTest {
+
+    @LocalServerPort
+    private int  port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void profile은_인증없이_호출된다() throws Exception{
+        String expected = "dafault";
+
+        ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(expected);
+
+    }
+}
